5ba7ce4ab102f012514fe17e690fc2fcfe6906c7
Juanchox06 <martinezsanchezjuansebastian7@gmail.com>
2025-10-10 06:18:28 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola doy el proyecto Club del remate");
+        System.out.println("hola soy el proyecto Club del remate");
     }
 }
